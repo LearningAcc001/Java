12b380945b1da47b55941cde08793253b3cc90b7
LearningAcc001 <99790994+LearningAcc001@users.noreply.github.com>
2022-10-12 09:45:15 +0800

    Update Class02.java

--- quiz/Class02.java
+++ quiz/Class02.java
@@ -1,33 +1,31 @@
-package 小考1;
-
 class Data{
     private String name;
-    private int score1;
-    private int score2;
+    private Test score;
+    private Test data;
 
     public Data(String n, int e, int m){
-        name=n;
-        score1=e;
-        score2=m;
+        this.name=n;
+        data=new Test(e,m);
     }
 
     class Test{
         int english;
         int math;
+        public Test(){
 
+        }
         public Test(int eng,int m){
             english = eng;
             math = m;
         }
+
         double avg(){
-            return (double)(english+math)/2;
+            return ((english+math)/2);
         }
     }
-    Test ss = new Test(score1,score2);
-    
 
     public void show(){
-        System.out.println("name:"+name+" english:"+score1+" math:"+score2+" avg:"+ss.avg());
+        System.out.println("name:"+name+" english:"+data.english+" math:"+data.math+" avg:"+data.avg());
     }
 
 }
@@ -35,11 +33,12 @@ class Data{
 public class Class02 {
     public static void main(String[] args) {
 
-        Data S1= new Data("Annie", 85, 95);
-        S1.show();
-        Data S2 = new Data("Brian", 77, 56);
-        S2.show();
+        Data[] a = new Data[2];
+        a[0] = new Data("Annie",85,92);
+        a[1] = new Data("Brian", 77, 56);
 
+        a[0].show();
+        a[1].show();
 
     }
 }
