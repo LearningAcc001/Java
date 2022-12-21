1f1a7fa45e441e5831564af11d74b1fede0d48f6
LearningAcc001 <99790994+LearningAcc001@users.noreply.github.com>
2022-12-21 11:29:51 +0800

    Update J3.java

--- Quiz02/J3.java
+++ Quiz02/J3.java
@@ -1,10 +1,11 @@
 package quiz2;
 
-interface IArea {  // IArea介面宣告// 介面方法: 計算面積
+interface IArea {  // IArea 介面宣告
+	// 介面方法: 計算面積
 	void area();
 }
 
-class Circle implements IArea { // Circle類別宣告
+class Circle implements IArea { // Circle 類別宣告
 	
 	private double r;     // 半徑
 	// 建構子
@@ -17,10 +18,9 @@ class Circle implements IArea { // Circle類別宣告
 	}
 }
 
-
-class Rectangle implements IArea {  // Rectangle類別宣告
+class Rectangle implements IArea {  // Rectangle 類別宣告
 	
-	private double width;     // 寬
+	private double width;     //寬
 	private double height;    //高
 	
 	// 建構子
@@ -28,47 +28,37 @@ class Rectangle implements IArea {  // Rectangle類別宣告
 		this.width = w;
 		this.height =h;
 	}
-	//請自行撰寫
 	
 	// 成員方法: 實作介面方法area()
 	public void area() {
 		
 		System.out.println("矩形面積: " + width*height);
 	}
-
 }
 
-
-class Triangle implements IArea {   // Triangle類別宣告
+class Triangle implements IArea {   // Triangle 類別宣告
 	private double height;    // 高
 	private double bottom;    // 三角形底長
 
-	// 建構子請自行撰寫
-	
+	// 建構子
 	public Triangle(double h, double b) {
 		this.height = h;
 		this.bottom = b;
 	}
 	
-	// 成員方法: 實作介面方法area()請自行撰寫
-	
+	// 成員方法: 實作介面方法area()
 	public void area() {
-		
 		System.out.println("三角形面積: "+bottom*height/2);
 	}
-
 }
 
 public class J3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	public static void main(String[] args) {		
 		IArea a;
 		Circle c = new Circle(6.0);
 		Rectangle r=new Rectangle(10.0, 15.0);
 		Triangle t=new Triangle(20.0, 15.0);
-		
 		
 		for ( int i = 1; i <= 3; i++ ) {
 			if ( i == 1 )        
@@ -78,8 +68,6 @@ public class J3 {
 			else          
 				a = t;  // 三角形
 			a.area();
-			
 		}
-		
 	}
 }
